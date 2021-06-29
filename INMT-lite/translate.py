8e690e66ef19a94283396678b0fab32172ead6a3
Mohd nad Zaki Rizvi <mohdsanadzakirizvi@gmail.com>
2021-06-29 09:17:41 +0000

    poc code for BBPE in inference and testing, needs to be debugged

--- INMT-lite/translate.py
+++ INMT-lite/translate.py
@@ -4,9 +4,10 @@ Code for performing inference/testing on the trained models.
 
 from utils.Model_architectures import Encoder, Decoder
 from utils.Model_types import Trans_model, Partial_model #TODO: Change it to better name
-from preprocess import preprocess_sentence, tokenize
-from tensorflow.keras.preprocessing.text import Tokenizer
+from preprocess import preprocess_sentence
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tokenizers import ByteLevelBPETokenizer
+from tokenizers.processors import TemplateProcessing
 
 import tensorflow as tf
 import numpy as np
@@ -23,6 +24,8 @@ def test(model, model_config, src_vocab, tgt_vocab, src_word_ind, tgt_word_ind, 
     dec_input =  tf.expand_dims([int(tgt_word_ind['<start>'])] * len(src_tensor), 1)
     print("predicting...")
     preds = model.predict([src_tensor, enc_hidden, dec_input])
+    print("%%%%%%%%", src_tensor[:5])
+    print("%%%%%%%% PRED", preds[:5])
     print("predicted")
     preds = np.array([preds[i].argmax(axis = 1) for i in range(len(preds))])
     preds = preds.swapaxes(0,1)
@@ -45,12 +48,13 @@ def test(model, model_config, src_vocab, tgt_vocab, src_word_ind, tgt_word_ind, 
             arr2.append(j)
             if tgt_vocab[str(j)] == '<end>':
                 break
-        
+        print("$$$$$$$$\n\n ARR1", arr1)
+        print("$$$$$$$$\n\n ARR2", arr2)
         temp = abs(len(arr1)-len(arr2))
         for j in range(min(len(arr1), len(arr2))):
             if arr1[j] == arr2[j]:
                 temp += 1    
-        score += temp
+        score += temp/len(arr1)
     
     test_acc = score/len(src_tensor)
     print("The Test accuracy on the given dataset: ", test_acc)
@@ -62,11 +66,17 @@ def inference(model, model_config, src_vocab, tgt_vocab, src_word_ind, tgt_word_
     preds = model.predict([src_tensor, enc_hidden, dec_input])
     preds = np.array([preds[i].argmax(axis = 1) for i in range(len(preds))])
     preds = preds.swapaxes(0,1)
-    file_path = os.path.join(tgt_path, 'inference.txt')
+    file_path = os.path.join('data', 'inference.txt')
     
+    # load BBPE tokenizer
+    lang_tokenizer = ByteLevelBPETokenizer(
+        './data/tgt_tokenizer_bpe-vocab.json',
+        './data/tgt_tokenizer_bpe-merges.txt',
+    )
+
     with open(file_path, 'w', encoding='utf-8') as f:
         for i in preds:
-            f.write(','.join([tgt_vocab[str(j)] for j in i]) + '\n')
+            f.write(lang_tokenizer.decode(i) + '\n')
     
     print("Results successfully saved at: ", file_path)
     
@@ -89,27 +99,37 @@ def inline(model, model_config, src_vocab, tgt_vocab, src_word_ind, tgt_word_ind
     
             
 
-def load_data(path, src_vocab, src_vocab_len, sen_len, padding = 'post'):
-    
-    
-    word_ind = {}
-    for i in src_vocab:
-        if i != 'num_words' and int(i) < src_vocab_len:
-            word_ind[src_vocab[i]] = i
-    
-    if path:
-    
-        lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
-        cleaned = [preprocess_sentence(i) for i in lines]
-        
-        tokenizer = Tokenizer(oov_token='<OOV>')
-        tokenizer.word_index = word_ind
-        tensor = tokenizer.texts_to_sequences(cleaned)
-        padded_tensor = pad_sequences(tensor, padding=padding, truncating='post', maxlen=sen_len)
-    
-        return padded_tensor, word_ind
+def load_data(path, vocab_path, merges_path, vocab_len, sent_len, padding = 'post'):
 
-    return None, word_ind
+    if path:
+
+        # load validation data
+        lines = io.open(path, encoding='UTF-8').read().strip().split('\n')
+        processed_text = [preprocess_sentence(i) for i in lines]
+
+        # load BBPE tokenizer
+        lang_tokenizer = ByteLevelBPETokenizer(
+            vocab_path,
+            merges_path,
+        )
+
+        # post tokenization processing
+        lang_tokenizer.post_processor = TemplateProcessing(
+            single="<start> $A <end>",
+            special_tokens=[
+                ("<start>", lang_tokenizer.token_to_id("<start>")),
+                ("<end>", lang_tokenizer.token_to_id("<end>")),
+            ],
+        )
+
+        # encoding the input into tokens using BBPE
+        tensor = lang_tokenizer.encode_batch(processed_text)
+        tensor = list(map(lambda x: x.ids, tensor))
+
+        # padd the input sentence
+        tensor = pad_sequences(tensor, padding=padding, truncating='post', maxlen=sent_len)
+
+        return tensor
     
     
     
@@ -119,23 +139,32 @@ def load_paths(model_path,
                 tgt_path, 
                 src_vocab_path,
                 tgt_vocab_path,
+                src_merges_path, 
+                tgt_merges_path,
                 model_config_path
                 ):
     src_vocab = None
     tgt_vocab = None
     model_config = None
     with open(src_vocab_path, encoding='utf-8') as f:
-        src_vocab = json.load(f)
+        src_word_ind = json.load(f)
+        src_word_ind = {k:str(v) for k,v in src_word_ind.items()}
+ 
+        src_vocab = {str(v):k for k,v in src_word_ind.items()}
     with open(tgt_vocab_path, encoding='utf-8') as f:
-        tgt_vocab = json.load(f)
+        tgt_word_ind = json.load(f)
+        tgt_word_ind = {k:str(v) for k,v in tgt_word_ind.items()}
+
+        tgt_vocab = {str(v):k for k,v in tgt_word_ind.items()}
     with open(model_config_path, encoding='utf-8') as f:
         model_config = json.load(f)
     
     src_vocab['0'] = '<pad>'
     tgt_vocab['0'] = '<pad>'
-        
-    src_vocab_len = src_vocab['num_words']
-    tgt_vocab_len = tgt_vocab['num_words']
+    # print("$$$$$$$$$$$\n\n TGT VOCAB", tgt_vocab)    
+    # print("$$$$$$$$$$$\n\n TGT WRD IDX", tgt_word_ind)    
+    src_vocab_len = len(src_vocab)
+    tgt_vocab_len = len(tgt_vocab)
     Tx = model_config['Tx']
     Ty = model_config['Ty']
     reccurent_hidden = model_config['reccurent_hidden']
@@ -144,12 +173,10 @@ def load_paths(model_path,
     
     src_tensor = None
     tgt_tensor = None
-    src_word_ind = None
-    tgt_word_ind = None
     
     
-    src_tensor, src_word_ind = load_data(src_path, src_vocab, src_vocab_len, Tx)
-    tgt_tensor, tgt_word_ind = load_data(tgt_path, tgt_vocab, tgt_vocab_len, Ty)
+    src_tensor = load_data(src_path, src_vocab_path, src_merges_path, src_vocab_len, Tx)
+    tgt_tensor = load_data(tgt_path, tgt_vocab_path, tgt_merges_path, tgt_vocab_len, Ty)
     
     encoder = Encoder(src_vocab_len, src_word_vec_size, reccurent_hidden)
     decoder = Decoder(tgt_vocab_len, tgt_word_vec_size, reccurent_hidden)
@@ -177,6 +204,8 @@ if __name__ == "__main__":
     mode = ''
     src_vocab_path = ''
     tgt_vocab_path = ''
+    src_merges_path = ''
+    tgt_merges_path = ''
     model_config_path = ''
     use_gpu = False
     
@@ -191,6 +220,8 @@ if __name__ == "__main__":
                                                  'mode=',
                                                  'src_vocab_path=',
                                                  'tgt_vocab_path=',
+                                                 'src_merges_path=',
+                                                 'tgt_merges_path=',
                                                  'use_gpu=',
                                                  'model_config_path=',
                                                  ])
@@ -207,6 +238,8 @@ if __name__ == "__main__":
         elif opt == '--mode': mode = arg
         elif opt == '--src_vocab_path': src_vocab_path = arg
         elif opt == '--tgt_vocab_path': tgt_vocab_path = arg
+        elif opt == '--src_merges_path': src_merges_path = arg
+        elif opt == '--tgt_merges_path': tgt_merges_path = arg
         elif opt == '--use_gpu': use_gpu = False if arg.lower() == "false" else True
         elif opt == '--model_config_path': model_config_path = arg
     
@@ -216,6 +249,8 @@ if __name__ == "__main__":
     if not(len(model_config_path)): raise AttributeError("Please provide path for model config file")
     if not(len(src_vocab_path)): raise AttributeError("Provide path for source language vocabulary")
     if not(len(tgt_vocab_path)): raise AttributeError("Provide path for target language vocabulary")
+    if not(len(src_merges_path)): raise AttributeError("Provide path for source language merge rules file of BPE")
+    if not(len(tgt_merges_path)): raise AttributeError("Provide path for target language merge rules file of BPE")
     if not(len(model_path)): raise AttributeError("Provide path for trained model")
     
     
@@ -231,6 +266,8 @@ if __name__ == "__main__":
                                                                                                         tgt_path, 
                                                                                                         src_vocab_path,
                                                                                                         tgt_vocab_path,
+                                                                                                        src_merges_path, 
+                                                                                                        tgt_merges_path,
                                                                                                         model_config_path
                                                                                                         )
         test(model, model_config, src_vocab, tgt_vocab, src_word_ind, tgt_word_ind, src_tensor, tgt_tensor)
@@ -241,6 +278,8 @@ if __name__ == "__main__":
                                                                                             '', 
                                                                                             src_vocab_path,
                                                                                             tgt_vocab_path,
+                                                                                            src_merges_path, 
+                                                                                            tgt_merges_path,
                                                                                             model_config_path
                                                                                             )
         # print(tgt_word_ind)
