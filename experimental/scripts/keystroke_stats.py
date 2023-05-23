1d6662ed2faef9c4e75897561ca97df24138cd4a
harshitadd <harshitadd@gmail.com>
2023-05-23 13:39:30 +0000

    Debugged keystroke analysis script

--- experimental/scripts/keystroke_stats.py
+++ experimental/scripts/keystroke_stats.py
@@ -27,37 +27,38 @@ for record in records:
     elif 'Next Word Dropdown' in i:
         interface_cluster['NWD'].append((t,log))
 
-
-# for key in interface_cluster.keys():  
-#     print(f'Computing Keystroke stats for {key} which has {len(interface_cluster[key])} records.')
-#     time_taken, nobp, tok, total_suggestions, tapped_suggestions, tidx = [], 0, 0, 1, 0, 0 # 1 to avoid underflow for the first 2 interfaces        
-#     for record in interface_cluster[key]:
-#         translation, logs = record[0], record[1]
-#         logs = logs.replace('""','"')
-#         logs = logs.strip('"')
-#         logs = json.loads(logs)
-        
-#         # Compute the total time taken to complete the translations 
-#         for ks, ts in enumerate(logs): 
-#             if 'button' in ts['message']:
-#                 time_taken.append(ts['message']['time_taken']//6000)
+for key in interface_cluster.keys():  
+    print(f'Computing Keystroke stats for {key} which has {len(interface_cluster[key])} records.')
+    time_taken, nobp, tok, total_suggestions, tapped_suggestions, tidx = [], 0, 0, 1, 0, 0 # 1 to avoid underflow for the first 2 interfaces        
+    for record in interface_cluster[key]:
+        translation, logs = record[0], record[1]
+        logs = logs.replace('""','"')
+        logs = logs.strip('"')
+        logs = json.loads(logs)
     
-#         # Compute the total number of keystrokes per interface 
-#         tok = len(logs)
-#         # Compute the number of backspaces per intefaces 
-#         for ks, ts in enumerate(logs): 
-#             for elements in ts['message']: 
-#                 try: 
-#                     if ts['message'][elements] == 'bp':
-#                         nobp += 1 
-#                 except: 
-#                     continue
+        
+        # Compute the total time taken to complete the translations 
+        for ks, ts in enumerate(logs): 
+            if 'button' in ts['message']:
+                time_taken.append(ts['message']['time_taken']/60000)
+                # time_taken = ts['message']['time_taken']//60000
+        
+        # Compute the total number of keystrokes per interface 
+        tok += len(logs) #Number of keystrokes per record 
+        # Compute the number of backspaces per intefaces 
+        for ks, ts in enumerate(logs): 
+            for elements in ts['message']: 
+                try: 
+                    if ts['message'][elements] == 'bp':
+                        nobp += 1 
+                except: 
+                    continue
                 
-#     # Printing Interface-Wide Stats 
-#     print(f'Found valid records for {len(time_taken)} records.')
-#     print(f'For interface {key}: the average time taken to complete a translation is {np.average(time_taken)}')
-#     print(f'For interface {key}: the average number of backspaces is is {nobp/len(time_taken)}')
-#     print(f'For interface {key}: the average number of keystrokes is {tok/len(time_taken)}')
+    # Printing Interface-Wide Stats 
+    print(f'Found valid records for {len(time_taken)} records.')
+    print(f'For interface {key}: the average time taken to complete a translation is {np.average(time_taken)}')
+    print(f'For interface {key}: the average number of backspaces is {nobp/len(time_taken)}')
+    print(f'For interface {key}: the average number of keystrokes is {tok/len(time_taken)}')
 
 
 for key in interface_cluster.keys():  
@@ -82,8 +83,8 @@ for key in interface_cluster.keys():
                         for seed in message[outer_seed]:
                             if seed == 'total_invocation_time': 
                                 total_suggestions += 1
-                except: 
-                    print(message) 
+                except Exception as e: 
+                    pass         
                     
                 if key == 'SBOW':     ## Specific computation for SBOW which does not have the total number of suggestions shown
                     total_suggestions = 0 
@@ -94,20 +95,17 @@ for key in interface_cluster.keys():
                             total_suggestions += len(record['BOW'])    
                     
         print(f'{total_suggestions} are total suggestions.')
-        print(f'{tidx} are tapped indices from earlier method.')
+        # print(f'{tidx} are tapped indices from earlier method.')
         print(f'{tapped_suggestions} are tapped indices from new method.')
-        print(f'For interface {key}: the average number of tapped indices by old method {(tidx/total_suggestions)*100}')
-        print(f'For interface {key}: the average number of tapped indices by new method {(tapped_suggestions/total_suggestions)*100}')
+        # print(f'For interface {key}: the average number of tapped indices by old method {(tidx/total_suggestions)*100}')
+        print(f'For interface {key}: the average number of tapped indices by new method {(tapped_suggestions/total_suggestions)}')
         print('*******************************************************************************************************************')
     else: 
         print('Not Applicable for this interface.')            
 
-
-   
-
-    # with open('analysis_stats.json', 'a') as f:
-    #     obj = {key: [np.average(time_taken), nobp/len(time_taken), tok/len(time_taken), (tapped_suggestions/total_suggestions)*100]}
-    #     f.write(json.dumps(obj, ensure_ascii=False) + '\n')
+    with open('analysis_stats.json', 'a') as f:
+        obj = {key: [np.average(time_taken), nobp/len(time_taken), tok/len(time_taken), (tapped_suggestions/total_suggestions)]}
+        f.write(json.dumps(obj, ensure_ascii=False) + '\n')
     
 
 
